c86ac6e5c7521b988c8518fd6d3ad6391c3e6ce4
Aaditya Bhetuwal <aaditya3210@gmail.com>
2023-04-03 22:11:58 +0530

    Fixed bugs

--- homepage.php
+++ homepage.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $currentTime = date("H:i");
 if ($currentTime >= "05:00" && $currentTime < "12:00") {
     $greeting = "Good morning";
@@ -11,78 +11,77 @@ if (isset($_SESSION['username'])) {
     $username = $_SESSION['username'];
 } else {
     $username = "";
-    $new="New to This Platform";
+    $new = "New to This Platform";
 }
 
 ?>
 
 
 <head>
-<style>
-    .section-styling:before {
-    content: "";
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: currentColor;
-    z-index: 1;
-}
-.section-styling {
-    min-height: 680px;
-}
-    .section-styling{
-        position: relative;
-    text-align: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding-bottom: 0;
-    }
+    <style>
+        .section-styling:before {
+            content: "";
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: currentColor;
+            z-index: 1;
+        }
 
-    .dark1 {
-  font-weight: 600;
-  background: linear-gradient(right, #080808, #2b2b2b, #080808);
-  color: #fff;
-  text-shadow: 1px 1px 0px rgba(0,0,0,0.7);
-}
-.dark1.header{
-    font-size: 70px;
-}  
+        .section-styling {
+            min-height: 680px;
+        }
 
-</style>
+        .section-styling {
+            position: relative;
+            text-align: center;
+            background-size: cover;
+            background-repeat: no-repeat;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding-bottom: 0;
+        }
+
+        .dark1 {
+            font-weight: 600;
+            background: linear-gradient(right, #080808, #2b2b2b, #080808);
+            color: #fff;
+            text-shadow: 1px 1px 0px rgba(0, 0, 0, 0.7);
+        }
+
+        .dark1.header {
+            font-size: 70px;
+        }
+    </style>
 </head>
 
 <body>
 
-<div class="home-content">
-    <h1 class="dark"></h1>
-    <?php if (!isset($_SESSION['username'])) { ?>
-        <a href="signup.php" class="btn btn-primary">Signup here</a>
-    <?php } ?>
-</div>
-<main class="page landing-page">
-<section class="section-styling" style="background-image:url('images/bgimage.jpg'); position:relative;">
-  <div style="background-color:rgba(0,0,0,0.5); position:absolute; top:0; left:0; width:100%; height:100%;"></div>
-  <div class="text" style="position:absolute; top:50%; left:50%; transform:translate(-50%,-50%); text-align:center; z-index:1;">
-    <h1 class="dark1 header"><?php echo $greeting . " " . $username; ?></h1>
-    <h2 class="dark1"><?php echo $new . " " . $username; ?></h2>
-    <?php if (!isset($_SESSION['username'])) { ?>
-    <button class="btn btn-primary"data-toggle="modal" data-target="#signupModal" type="button">Signup here</button>
-    <?php } ?>
-    <br>
-    <br>
-    <h2 class="dark1"><?php echo "Ask Your Questions Here "; ?></h2><br>
-    <button class="btn btn-primary" href="signup.php" type="button">Ask</button>
-  </div>
-</section>
+    <div class="home-content">
+        <h1 class="dark"></h1>
+        <!-- <?php if (!isset($_SESSION['username'])) { ?>
+            <a href="signup.php" class="btn btn-primary">Signup here</a>
+        <?php } ?> -->
+    </div>
+
+    <main class="page landing-page">
+        <section class="section-styling" style="background-image:url('images/bgimage.jpg'); position:relative;">
+            <div style="background-color:rgba(0,0,0,0.5); position:absolute; top:0; left:0; width:100%; height:100%;"></div>
+            <div class="text" style="position:absolute; top:50%; left:50%; transform:translate(-50%,-50%); text-align:center; z-index:1;">
+                <h1 class="dark1 header"><?php echo $greeting . " " . $username; ?></h1>
+                <h2 class="dark1"><?php echo $new . " " . $username; ?></h2>
+                <?php if (!isset($_SESSION['username'])) { ?>
+                    <button class="btn btn-primary" data-toggle="modal" data-target="#signupModal" type="button">Signup here</button>
+                <?php } ?>
+                <br>
+                <br>
+                <h2 class="dark1"><?php echo "Ask Your Questions Here "; ?></h2><br>
+                <button class="btn btn-primary" href="signup.php" type="button">Ask</button>
+            </div>
+        </section>
     </main>
 
 
-    </body>
-
-
-
-
+</body>
--- index.php
+++ index.php
@@ -5,9 +5,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css"
-        integrity="sha256-2XFplPlrFClt0bIdPgpz8H7ojnk10H69xRqd9+uTShA=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" integrity="sha256-2XFplPlrFClt0bIdPgpz8H7ojnk10H69xRqd9+uTShA=" crossorigin="anonymous" />
     <title>KnowThis</title>
+
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="assets/css/vanilla-zoom.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+    <link rel="stylesheet" href="https://cdn.tailwindcss.com/3.0.12">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.1.0/mdb.min.css" rel="stylesheet" />
+
+
     <style>
         body {
             margin-top: 20px;
